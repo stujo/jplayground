45e897617f7fa674eee995593b87c39af9fa6e5f
stujo <stujo@imagescript.com>
2014-05-16 13:35:49 -0700

    PreK exercises first pass

--- app/assets/javascripts/grounds/prek.js
+++ app/assets/javascripts/grounds/prek.js
@@ -1,10 +1,23 @@
+"use strict;"
 /* Javascript for playground: Pre-K */
 
 $( document ).ready(function() {
 
 
-// Your Code Here
+// A) Email Address Field
+// 1) Assign emailField to be the jQuery object for the input field with id='email_address'
+  var emailField = null;
 
+// 2) Change the placeholder attribute of the field to "Email Address"
+
+// B) Chang CSS Dynamically
+// 1) Assign greenThing to be the jQuery object for the div with id green_thing
+
+  var greenThing = null;
+
+// 2) Assign the css class 'jplay_green' to greenThing
+
+// 3) Change the HTML of the greenThing to be I'm Green
 
 });
 
