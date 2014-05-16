3fdb9e896b909e033fd54ef32aa3faa2a99dacee
stujo <stujo@imagescript.com>
2014-05-16 11:59:54 -0700

    Autocreate playground files

--- app/controllers/playgrounds_controller.rb
+++ app/controllers/playgrounds_controller.rb
@@ -10,14 +10,25 @@ class PlaygroundsController < ApplicationController
     define_method(key) do
       @title = value
       @playgrounds = PLAYGROUNDS
-      unless template_exists?(key)
-        view_filename = Rails.root.join("app", "views", params[:controller], "#{key}.html.erb")
-        File.open view_filename, 'w' do |f|
-          f.write "<h1>#{value}</h1>\n\n"
-          f.write "<div>Make your playground by editting #{view_filename}</div>\n\n"
-        end
-      end
+      @playground_name = key
       render key
+    end
+
+    view_filename = Rails.root.join("app", "views", "playgrounds", "#{key}.html.erb")
+    unless File.exist?(view_filename)
+      File.open view_filename, 'w' do |f|
+        f.write "<h1>#{value}</h1>\n\n"
+        f.write "<div>Make your playground by editting #{view_filename}</div>\n\n"
+      end
+    end
+
+    js_filename = Rails.root.join("app", "assets", "javascripts", "#{key}.js")
+
+    unless File.exist?(js_filename)
+      File.open view_filename, 'w' do |f|
+        f.write "/* Javascript for playground: #{value} */\n\n"
+        f.write "$( document ).ready(function() {\n\n\n// Your Code Here\n\n\n});\n\n"
+      end
     end
   end
 end
