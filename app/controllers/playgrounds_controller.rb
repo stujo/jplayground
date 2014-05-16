cb3ff38b72f5e798bda340715d7b50da269b4b89
stujo <stujo@imagescript.com>
2014-05-16 11:44:32 -0700

    Autocreate Views for playgrounds

--- app/controllers/playgrounds_controller.rb
+++ app/controllers/playgrounds_controller.rb
@@ -1,2 +1,22 @@
 class PlaygroundsController < ApplicationController
+
+  def index
+    @playgrounds = PLAYGROUNDS
+  end
+
+  PLAYGROUNDS = {:prek => 'Pre-K', :junior => 'Junior'}
+
+  PLAYGROUNDS.each do |key, value|
+    define_method(key) do
+      @title = value
+      @playgrounds = PLAYGROUNDS
+      unless template_exists?(key)
+        view_filename = Rails.root.join("app", "views", params[:controller], "#{key}.html.erb")
+        File.open view_filename, 'w' do |f|
+          f.write "<h1>#{value}</h1>\n\n"
+        end
+      end
+      render key
+    end
+  end
 end
